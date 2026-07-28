7c0004876bee129cbb77b2db857539461f2ac6e2
Denis Cooper <denis.cooper09@gmail.com>
2026-07-28 16:58:27 +0100

    Get RapidXml working

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
+#include <fstream>
 #include <iostream>
+#include <vector>
+#include <rapidxml/rapidxml.hpp>
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    std::ifstream svd("../../test/STM32F103.svd");
+
+    std::vector buffer((std::istreambuf_iterator(svd)), std::istreambuf_iterator<char>());
+    buffer.push_back('\0');
+
+    rapidxml::xml_document doc;
+    doc.parse<0>(buffer.data());
+
+    auto* root = doc.first_node("device");
+    auto* name = root->first_node("name");
+    std::cout << name->value() << '\n';
+
     return 0;
 }
